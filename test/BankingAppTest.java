15c8f57efb3c591c1ef758daa784fbca89bb4f27
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-03-21 02:19:02 +0100

    newDeitel

--- test/BankingAppTest.java
+++ test/BankingAppTest.java
@@ -7,18 +7,21 @@ class BankingAppTest {
     @Test
     void checkDepositingAmount() {
         BankingApp myBankApp = new BankingApp();
+        myBankApp.setPin("1234");
         assertEquals(2000, myBankApp.depositCash(2000));
     }
 
     @Test
     void checkThatAccountCanDepositZeroAmount() {
         BankingApp myBankApp = new BankingApp();
-        assertEquals(0, myBankApp.depositCash(0));
+        myBankApp.setPin("1234");
+        assertEquals(0, myBankApp.depositCash(0.00));
     }
 
     @Test
     void checkThatMyBankAppCannotDepositMinusValue() {
         BankingApp myBankApp = new BankingApp();
+        myBankApp.setPin("1234");
         assertEquals(2000, myBankApp.depositCash(2000));
 
     }
@@ -33,25 +36,38 @@ class BankingAppTest {
     void checkThatTheAccountIsAbleToReceiveMoney() {
         BankingApp myBank = new BankingApp();
         myBank.depositCash(1000);
-       assertEquals(800, myBank.withdrawCash(200));
-        assertEquals(0,myBank.withdrawCash(-1));
+        myBank.setPin("1234");
+       assertEquals(800, myBank.withdrawCash(200,"1234"));
+        assertEquals(0,myBank.withdrawCash(-1,"1234"));
     }
     @Test
     void checkThatAccountCanBuyAirtime() {
         BankingApp myBank = new BankingApp();
         myBank.depositCash(1000);
-        assertEquals(800, myBank.buyAirtime(200));
+        myBank.setPin("1234");
+        assertEquals(800, myBank.buyAirtime(200,"1234"));
     }
 @Test
     void checkThatAccountCanTransfer(){
         BankingApp myBank=new BankingApp();
         myBank.depositCash(1000);
-        assertEquals(990,myBank.transferMoney(10));
+    myBank.setPin("1234");
+        assertEquals(990,myBank.transferMoney(10,"1234"));
 }
 @Test
     void checkThatAccountCannotTransferNegativeValues(){
         BankingApp myBank = new BankingApp();
         myBank.depositCash(1000);
-        assertEquals(900,myBank.transferMoney(100));
+    myBank.setPin("1234");
+        assertEquals(900,myBank.transferMoney(100,"1234"));
+}
+
+@Test
+ void checkForPin(){
+        BankingApp myBank=new BankingApp();
+        myBank.setPin("123");
+        myBank.depositCash(1000);
+        assertEquals(900, myBank.withdrawCash(100,"123"));
+
 }
 }
